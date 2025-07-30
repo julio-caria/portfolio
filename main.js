7ab9b1acc4e0de91f4a6f7fb394cd439f3510d93
Júlio Caria <julio.caria@hotmail.com>
2025-07-29 23:56:51 -0300

    feat: show more projects button

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const searchInput = document.getElementById('search-project');
 const projectItems = document.querySelectorAll('.project-item');
 const filterBtns = document.querySelectorAll('.filter-btn')
 
+const descriptionProject = document.querySelectorAll('.description-project');
+
 let selectedCategory = 'all'
 
 function filterProjects() {
@@ -29,4 +31,23 @@ filterBtns.forEach(filter => {
     selectedCategory = filter.getAttribute('data-filter');
     filterProjects();
   })
+})
+
+descriptionProject.forEach(desc => {
+  const btn = desc.parentElement.querySelector('.btn-see-more');
+  function isOverflowing(element) {
+    return element.scrollHeight > element.clientHeight
+  }
+  if(isOverflowing(desc)) {
+    btn.style.display = 'inline-block';
+    btn.addEventListener('click', function() {
+      if(desc.classList.contains('ellipsis-multiline')) {
+        desc.classList.remove('ellipsis-multiline');
+        btn.textContent = 'Ver menos';
+      } else {
+        desc.classList.add('ellipsis-multiline');
+        btn.textContent = 'Ver mais'
+      }
+    })
+  }
 })
